a408c2dd354049bb8b1d1435e9f90e82dda0f672
Toluene1 <adaramoti111@gmail.com>
2023-01-27 14:57:42 +0100

    home page set up completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,35 @@ function App() {
   return (
     <section>
       <main>
-        <div className="logoHome"></div>
+        <div className="logoHome">
+          <img src="Union.png" className="unionLogo" alt="" />
+          <img src="lendsqr.png" className="lendLogo" alt="" />
+        </div>
       </main>
       <main className="divHome">
-        <div className="homeDivs">{/* <img src="pablo.png" alt="" /> */}</div>
-        <div className="homeDivs2">Tolu</div>
+        <div className="homeDivs"></div>
+        <div className="homeDivs2">
+          <h3 className="welcome">Welcome!</h3>
+          <p className="pEnter">Enter details to login</p>
+          <div className="inputDiv">
+            <input
+              type="text"
+              className="input1"
+              name=""
+              id=""
+              placeholder="Email"
+            />{" "}
+            <input
+              type="text"
+              className="input1"
+              name=""
+              id=""
+              placeholder="Password"
+            />
+          </div>
+          <p className="pEnter">forgot password?</p>
+          <button className="buttonHome">login</button>
+        </div>
       </main>
     </section>
   );
